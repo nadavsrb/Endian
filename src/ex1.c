5533ab8e486289e591dbbb9dafd16377d5961bd8
nadavsrb <nadavsrb@gmail.com>
2020-11-19 07:10:40 +0200

    now we are not using internal libraries

--- src/ex1.c
+++ src/ex1.c
@@ -1,9 +1,6 @@
 // 213056153 Nadav Sharabi
 #include "ex1.h"
 
-#include <stdint.h>
-#include <limits.h>
-
 #define NUM_BITES_IN_ONE_BYTE 8
 #define BYTE_CONTAINS_NUMBER_ONE 0x01
 #define BYTE_ALL_BITES_ARE_ONE 0xFF
@@ -27,16 +24,22 @@ int is_big_endian() {
     //than we would change the pointer type to uint_8 *,
     //so then we can use *, than whats happens is that we are
     //getting the byte in one that is in the lowest address.
-    uint8_t firtBit = *((uint8_t *)&one);
+    unsigned char firstBit = *((unsigned char *)&one);
 
     //if the firstByte is 0x01 then the byte in
     //the lowest address of one is LSB so we are in little endian.
     //, else its big endian.
-    if(firtBit == BYTE_CONTAINS_NUMBER_ONE){
+    if(firstBit == BYTE_CONTAINS_NUMBER_ONE){
         return 0;
     }
 
     return 1;
+}
+
+unsigned long getULMax() {
+    unsigned long zero = 0;
+
+    return ~zero;
 }
 
 unsigned long merge_bytes(unsigned long x, unsigned long int y){
@@ -44,8 +47,10 @@ unsigned long merge_bytes(unsigned long x, unsigned long int y){
     //here we are calculating the size of half unsigned long in bytes
     unsigned long halfSizeOfULInBites = (sizeof(unsigned long) * NUM_BITES_IN_ONE_BYTE) / 2;
 
-    unsigned long firstHalf = ULONG_MAX>>halfSizeOfULInBites;
-    unsigned long secondHalf = ULONG_MAX<<halfSizeOfULInBites;
+    const unsigned long uLMax= getULMax();
+
+    unsigned long firstHalf = uLMax>>halfSizeOfULInBites;
+    unsigned long secondHalf = uLMax<<halfSizeOfULInBites;
 
     firstHalf = y & firstHalf;
     secondHalf = x & secondHalf;
